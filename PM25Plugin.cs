05b8604a3c88025b23ad72ad45e86f48039332c0
P <p16181@gmail.com>
2021-03-16 23:49:02 +0700

    Hw3 620615027 Peeranat Keelapang

--- PM25Plugin.cs
+++ PM25Plugin.cs
@@ -51,15 +51,17 @@ namespace DNWS
             }
 
             HTTPResponse response = null;
-            StringBuilder sb = new StringBuilder();
-            sb.Append("<html><body>");
-            sb.Append("Location : " + pm25reading.data.city.name + "<br />");
-            sb.Append("Date/Time : " + pm25reading.data.time.iso + "<br />");
-            sb.Append("PM25 : " + pm25reading.data.iaqi.pm25.v + "<br />");
-            sb.Append("PM10 : " + pm25reading.data.iaqi.pm10.v + "<br />");
-            sb.Append("</body></html>");
+            //StringBuilder sb = new StringBuilder();
+            string jsonString = JsonSerializer.Serialize<PM25Reading>(pm25reading);
+            // sb.Append("<html><body>");
+            // sb.Append("Location : " + pm25reading.data.city.name + "<br />");
+            // sb.Append("Date/Time : " + pm25reading.data.time.iso + "<br />");
+            // sb.Append("PM25 : " + pm25reading.data.iaqi.pm25.v + "<br />");
+            // sb.Append("PM10 : " + pm25reading.data.iaqi.pm10.v + "<br />");
+            // sb.Append("</body></html>");
             response = new HTTPResponse(200);
-            response.body = Encoding.UTF8.GetBytes(sb.ToString());
+            //response.body = Encoding.UTF8.GetBytes(sb.ToString());
+            response.body = Encoding.UTF8.GetBytes(jsonString);
             return response;
         }
 
